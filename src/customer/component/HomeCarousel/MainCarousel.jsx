a34da6b3aca725311a3c69c89fe8990b7cc939ba
hidayatmahetar0 <hidayatmahetar818@gmail.com>
2023-12-12 22:20:05 +0530

    carousel add

--- src/customer/component/HomeCarousel/MainCarousel.jsx
+++ src/customer/component/HomeCarousel/MainCarousel.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import React from "react";
+import { mainCarouselData } from "./MainCarouselData";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
-
-
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
-
-const MainCarousel = () => (
-    <AliceCarousel
-        mouseTracking
-        items={items}
-        controlsStrategy="alternate"
+const MainCarousel = () => {
+  const items = mainCarouselData.map((item) => (
+    <img
+      className="coursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt=""
     />
-);
+  ));
+
+  return <AliceCarousel items={items} 
+  disableButtonsControls
+  autoPlay
+  autoPlayInterval={1000}
+  infinite
+  />;
+};
 
 export default MainCarousel;
--- src/customer/component/HomeCarousel/MainCarouselData.js
+++ src/customer/component/HomeCarousel/MainCarouselData.js
@@ -1,11 +1,10 @@
-export const gounsPage1=[
+export const mainCarouselData=[
     {
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/ /9_8. jpg",
-        path:"/women/clothing/women dress"
-
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
     },
+    
     {
         image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg",
-        path:" /women/clothing/women_saree"
+        // path:" /women/clothing/women_saree"
     }
 ]
