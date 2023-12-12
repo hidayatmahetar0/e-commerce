353cc25339cce4e8dc87d027e80d0a212d97ca44
hidayatmahetar0 <hidayatmahetar818@gmail.com>
2023-12-12 19:40:20 +0530

    add header and home section

---
+++ src/customer/component/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,11 @@
+export const gounsPage1=[
+    {
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/ /9_8. jpg",
+        path:"/women/clothing/women dress"
+
+    },
+    {
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg",
+        path:" /women/clothing/women_saree"
+    }
+]
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainCarousel from "../../component/HomeCarousel/MainCarousel";
+
+function HomePage() {
+  return (
+    <div>
+      <MainCarousel />
+
+      <div>other section</div>
+    </div>
+  );
+}
+
+export default HomePage;
